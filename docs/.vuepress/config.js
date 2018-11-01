833772a58fe3253b295754aba9b66b1f0f59f681
shipengqi <pooky.shipengqi@gmail.com>
2018-11-01 18:39:25 +0800

    Add logo and config file

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  base: '/vault-docs-Zh-CN/',
+  title: 'Vault 中文文档',
+  description: '欢迎来到 HashiCorp Vault 介绍指南!本指南是开始使用 Vault 的最佳地点。本指南涵盖了Vault 是什么，它可以解决什么问题，它如何与现有软件进行比较，\n' +
+  '并包含了使用 Vault 的快速入门。',
+  head: [
+    ['link', {
+      rel: 'icon',
+      href: '/logo.png'
+    }]
+  ],
+  markdown: {
+    toc: {
+      includeLevel: [2, 3, 4, 5, 6]
+    }
+  },
+  themeConfig: {
+    repo: 'shipengqi/vault-docs-Zh-CN',
+    docsDir: 'docs',
+    editLinks: true,
+    editLinkText: '错别字纠正',
+    sidebarDepth: 3,
+    nav: [{
+      text: 'Vault 入门',
+      link: '/getting-started',
+    },{
+      text: 'Vault 文档',
+      link: '/installing',
+    }],
+    sidebar: {
+      '/getting-started': [{
+        title: 'Vault 入门（持续更新中...）',
+        children: [
+          ''
+        ]
+      }],
+      '/installing': [{
+        title: 'Vault 文档（持续更新中...）',
+        children: [
+          ''
+        ]
+      }]
+    }
+  }
+};
